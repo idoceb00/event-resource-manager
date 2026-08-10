dcb0a0b08dfec9b6d9c5de74db8af935c2d9eb30
idoceb00 <idoceb00@gmail.com>
2026-08-10 17:20:07 +0200

    build: scaffold Spring Boot project with Maven and Lombok

---
+++ backend/src/main/java/com/idoceb00/eventory/backend/EventResoruceManagerBackendApplication.java
@@ -0,0 +1,12 @@
+package com.idoceb00.eventory.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventResoruceManagerBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EventResoruceManagerBackendApplication.class, args);
+  }
+}
